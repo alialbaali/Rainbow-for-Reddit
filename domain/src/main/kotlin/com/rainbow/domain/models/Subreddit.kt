af88027af4d2479938712787a04fc1b749083fda
Ali Albaali <ali@albaali.com>
2020-11-16 10:23:32 +0300

    Add Subreddit model

---
+++ domain/src/main/kotlin/com/rainbow/domain/models/Subreddit.kt
@@ -0,0 +1,46 @@
+package com.rainbow.domain.models
+
+import com.rainbow.domain.colorOf
+import kotlinx.datetime.LocalDate
+
+private val DefaultPrimaryColor = colorOf(red = "00", green = "00", blue = "00")
+private val DefaultBackgroundColor = colorOf(red = "FF", green = "FF", blue = "FF")
+private val DefaultKeyColor = colorOf(red = "FF", green = "FF", blue = "FF")
+
+data class Subreddit(
+    val id: String,
+    val name: String,
+    val title: String,
+    val description: String,
+    val subscribersCount: Long,
+    val activeSubscribersCount: Long,
+    val colors: Colors = DefaultColors,
+    val isNSFW: Boolean = false,
+    val creationDate: LocalDate,
+) {
+    data class Colors(
+        val primary: Long,
+        val background: Long,
+        val key: Long,
+    )
+}
+
+private val DefaultColors = Subreddit.Colors(DefaultPrimaryColor, DefaultBackgroundColor, DefaultKeyColor)
+
+val Subreddit.redditName
+    get() = "r/$name"
+
+val Subreddit.redditId
+    get() = "t5_$id"
+
+val Subreddit.redditUrl
+    get() = "/r/$name"
+
+val Subreddit.primaryColor
+    get() = colors.primary
+
+val Subreddit.backgroundColor
+    get() = colors.background
+
+val Subreddit.keyColor
+    get() = colors.key
