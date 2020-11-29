0d62714edaa45266ed57ebc9fd2897257a687919
Ali Albaali <ali@albaali.com>
2020-11-30 00:48:59 +0300

    Add Scope class for reddit permissions

---
+++ remote/src/main/kotlin/com/rainbow/remote/dto/Scope.kt
@@ -0,0 +1,39 @@
+package com.rainbow.remote.dto
+
+import com.rainbow.remote.lowerCaseName
+
+enum class Scope {
+    ModPosts, ModWiki, MySubreddits,
+    PrivateMessages, Read, Report,
+    History, ModConfig, ModFlair,
+    Vote, WikiEdit, WikiRead,
+    Save, Submit, Subscribe,
+    Identity, Edit, Flair;
+
+    companion object {
+
+        val UserScope = listOf(
+            PrivateMessages, Subscribe,
+            Submit, Vote, MySubreddits,
+            History, WikiRead, Flair,
+            Identity, Edit, Read,
+            Report, Save, Submit,
+        )
+
+        val ModScope = listOf(ModConfig, ModFlair, ModPosts, ModWiki, WikiEdit)
+
+        val All = listOf(
+            ModPosts, ModWiki, WikiEdit,
+            Submit, Vote, MySubreddits,
+            PrivateMessages, Subscribe,
+            History, WikiRead, Flair,
+            Identity, Edit, Read,
+            Report, Save, Submit,
+            ModConfig, ModFlair,
+        )
+
+        fun List<Scope>.asRedditScope() = joinToString(separator = " ") { scope -> scope.lowerCaseName }
+
+    }
+
+}
