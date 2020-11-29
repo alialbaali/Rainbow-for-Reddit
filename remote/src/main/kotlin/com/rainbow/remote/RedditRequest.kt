ccc5ca6651d56c49d66d040c09312d13f2f583d6
Ali Albaali <ali@albaali.com>
2020-11-30 00:44:51 +0300

    Add custom request methods for reddit api

--- remote/src/main/kotlin/com/rainbow/remote/RedditRequest.kt
+++ remote/src/main/kotlin/com/rainbow/remote/RedditRequest.kt
@@ -3,23 +3,76 @@ package com.rainbow.remote
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 
-internal suspend inline fun <reified T> HttpClient.redditRequest(
-    endpoint: RedditEndpoint,
+suspend inline fun <reified T> HttpClient.redditRequest(
+    url: String,
     builder: HttpRequestBuilder.() -> Unit = {}
-): RedditResponse<T> {
-    return request<HttpResponse> {
-        method = endpoint.method
-        url.encodedPath = endpoint.path
-        builder()
-    }.let { response ->
-        if (response.status.isSuccess())
-            response.receive<RedditResponse.Success<T>>()
-                .also { println("Success Response = $it") }
-        else
-            response.receive<RedditResponse.Failure<T>>()
-                .also { println("Failure Response = $it") }
-    }
+): RedditResponse<T> = request<HttpResponse> {
+    bearerAuthHeader(getLocalToken())
+    url("https://oauth.reddit.com/$url.json")
+    builder()
+}.asRedditResponse()
+
+
+suspend inline fun <reified T> HttpClient.customRedditRequest(
+    url: String,
+    builder: HttpRequestBuilder.() -> Unit = {}
+): T? = request {
+    bearerAuthHeader(getLocalToken())
+    url("https://oauth.reddit.com/$url.json")
+    builder()
 }
+
+
+suspend inline fun <reified T> HttpClient.redditSubmitForm(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit = {}
+): T = submitForm {
+    bearerAuthHeader(getLocalToken())
+    url("https://oauth.reddit.com/$url")
+    block()
+}
+
+suspend inline fun <reified T> HttpClient.redditGet(
+    urlString: String,
+    builder: HttpRequestBuilder.() -> Unit = {}
+): RedditResponse<T> = redditRequest(urlString) {
+    method = HttpMethod.Get
+    builder()
+}
+
+suspend inline fun <reified T> HttpClient.redditPost(
+    urlString: String,
+    builder: HttpRequestBuilder.() -> Unit = {}
+): RedditResponse<T> = redditRequest(urlString) {
+    method = HttpMethod.Post
+    builder()
+}
+
+suspend inline fun <reified T> HttpClient.redditPut(
+    urlString: String,
+    builder: HttpRequestBuilder.() -> Unit = {}
+): RedditResponse<T> = redditRequest(urlString) {
+    method = HttpMethod.Put
+    builder()
+}
+
+suspend inline fun <reified T> HttpClient.redditDelete(
+    urlString: String,
+    builder: HttpRequestBuilder.() -> Unit = {}
+): RedditResponse<T> = redditRequest(urlString) {
+    method = HttpMethod.Delete
+    builder()
+}
+
+suspend inline fun <reified T> HttpResponse.asRedditResponse(): RedditResponse<T> =
+    if (status.isSuccess())
+        receive<RedditResponse.Success<T>>()
+    else
+        receive<RedditResponse.Failure<T>>()
+
+
+fun getLocalToken() = "383986809160-W8fkmrvNV_VrDouPMun37hc9J20Mtg"
