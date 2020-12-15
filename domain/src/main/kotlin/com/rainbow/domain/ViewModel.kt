f61559a32f8d8b3943581f04420dd8deca0c4a55
Ali Albaali <ali@albaali.com>
2020-12-15 14:14:00 +0300

    Add ViewModel abstract class with MVI implementation to be used as the base for all ViewModels in the app

---
+++ domain/src/main/kotlin/com/rainbow/domain/ViewModel.kt
@@ -0,0 +1,34 @@
+package com.rainbow.domain
+
+import com.rainbow.domain.ViewModel.Intent
+import com.rainbow.domain.ViewModel.State
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+abstract class ViewModel<in I : Intent, out S : State>(initialState: S) {
+
+    val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    private val mutableIntent = MutableSharedFlow<I>(replay = 1)
+
+    private val mutableState = MutableStateFlow(initialState)
+    val state = mutableState.asStateFlow()
+
+    init {
+        mutableIntent
+            .onEach { intent -> mutableState.value = intent.onIntent() }
+            .launchIn(viewModelScope)
+    }
+
+    fun emitIntent(intent: I) = viewModelScope.launch { mutableIntent.emit(intent) }
+
+    protected abstract suspend fun I.onIntent(): S
+
+    interface Intent
+
+    interface State
+
+}
