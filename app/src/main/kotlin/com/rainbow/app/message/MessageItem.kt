21e567ef243d8f1e81b007a18b401fd9347ec1d5
Ali Albaali <ali@albaali.com>
2021-07-20 16:38:16 +0300

    Add MessageItem

---
+++ app/src/main/kotlin/com/rainbow/app/message/MessageItem.kt
@@ -0,0 +1,12 @@
+package com.rainbow.app.message
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rainbow.domain.models.Message
+
+@Composable
+fun MessageItem(message: Message, modifier: Modifier = Modifier) {
+
+
+
+}
