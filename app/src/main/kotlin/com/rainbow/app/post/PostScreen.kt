7e72e27304a13e90f96d12778b49ccdb0baefeb8
Ali Albaali <ali@albaali.com>
2021-11-09 02:29:09 +0300

    Refactor PostScreen

--- app/src/main/kotlin/com/rainbow/app/post/PostScreen.kt
+++ app/src/main/kotlin/com/rainbow/app/post/PostScreen.kt
@@ -1,45 +1,111 @@
 package com.rainbow.app.post
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import com.rainbow.app.comment.CommentContent
+import androidx.compose.ui.unit.dp
+import com.rainbow.app.PagingEffect
+import com.rainbow.app.comment.PostCommentItem
+import com.rainbow.app.components.RainbowProgressIndicator
+import com.rainbow.app.utils.ShapeModifier
 import com.rainbow.app.utils.UIState
-import com.rainbow.app.utils.composed
+import com.rainbow.app.utils.defaultPadding
 import com.rainbow.app.utils.toUIState
 import com.rainbow.data.Repos
+import com.rainbow.domain.models.Comment
 import com.rainbow.domain.models.Post
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 
 @Composable
-fun PostScreen(postId: String, modifier: Modifier = Modifier) {
-
-    val scope = rememberCoroutineScope()
-
-    val state by produceState<UIState<Post>>(UIState.Loading, postId) {
-        Repos.Post.getPost(postId)
+fun PostScreen(
+    post: Post,
+    modifier: Modifier = Modifier,
+    onUserNameClick: (String) -> Unit,
+    onSubredditNameClick: (String) -> Unit,
+) {
+    var lastComment by remember { mutableStateOf<Comment?>(null) }
+    val scrollingState = rememberLazyListState()
+    val state by produceState<UIState<List<Comment>>>(UIState.Loading, post.id) {
+        Repos.Comment.getPostsComments(post.id)
             .map { it.toUIState() }
             .collect { value = it }
     }
+    LazyColumn(
+        modifier
+            .then(ShapeModifier)
+            .defaultPadding(),
+        scrollingState
+    ) {
+        item {
+            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                PostInfo(
+                    post = post,
+                    onUserNameClick,
+                    onSubredditNameClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
 
-    state.composed { post ->
+                Spacer(Modifier.height(8.dp))
 
-        CommentContent(postId, modifier) {
+                PostTitle(
+                    title = post.title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
 
-//            PostItem(
-//                post,
-//                onUpvote = { scope.launch { Repos.Post.upvotePost(postId) } },
-//                onDownvote = { scope.launch { Repos.Post.downvotePost(postId) } },
-//                onUnvote = { scope.launch { Repos.Post.unvotePost(postId) } },
-//                onShare = {},
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .wrapContentHeight(),
-//            )
+                Spacer(Modifier.height(8.dp))
 
+                PostContent(
+                    post = post,
+                    modifier = Modifier
+                        .heightIn(max = 600.dp)
+                        .fillMaxWidth()
+                )
+
+                Spacer(Modifier.height(8.dp))
+
+                PostCommands(
+                    post,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
+            }
+        }
+        item { Divider(Modifier.padding(vertical = 16.dp)) }
+        comments(state, onLoadMore = { lastComment = it })
+    }
+    VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
+}
+
+private fun LazyListScope.comments(
+    commentsState: UIState<List<Comment>>,
+    onLoadMore: (Comment) -> Unit,
+) {
+    when (commentsState) {
+        is UIState.Empty -> item { Text("No comments found.") }
+        is UIState.Failure -> item { Text("Failed to load comments") }
+        is UIState.Loading -> item { RainbowProgressIndicator() }
+        is UIState.Success -> {
+            val comments = commentsState.value
+            itemsIndexed(comments) { index, comment ->
+                PostCommentItem(comment)
+                PagingEffect(comments, index, onLoadMore)
+                if (comment.replies.isNotEmpty())
+                    this@comments.comments(UIState.Success(comment.replies), onLoadMore)
+            }
         }
     }
 }
