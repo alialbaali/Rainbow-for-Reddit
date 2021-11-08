0651d4a4b2872d205fc9d34944e40ab9647a6115
Ali Albaali <ali@albaali.com>
2021-11-09 02:40:22 +0300

    Make UIState sealed interface and Add Empty state

--- app/src/main/kotlin/com/rainbow/app/utils/UIState.kt
+++ app/src/main/kotlin/com/rainbow/app/utils/UIState.kt
@@ -3,10 +3,11 @@ package com.rainbow.app.utils
 import com.rainbow.app.utils.UIState.*
 
 
-sealed class UIState<out T> {
-    object Loading : UIState<Nothing>()
-    data class Success<T>(val value: T) : UIState<T>()
-    data class Failure(val exception: Throwable) : UIState<Nothing>()
+sealed interface UIState<out T> {
+    object Empty : UIState<Nothing>
+    object Loading : UIState<Nothing>
+    data class Success<T>(val value: T) : UIState<T>
+    data class Failure(val exception: Throwable) : UIState<Nothing>
 
     val isLoading
         get() = this is Loading
@@ -20,28 +21,33 @@ sealed class UIState<out T> {
 }
 
 fun <T> UIState<T>.getOrNull(): T? = when (this) {
+    is Empty -> null
     is Loading -> null
     is Success -> value
     is Failure -> null
 }
 
 fun <T> UIState<T>.getOrDefault(defaultValue: T): T = when (this) {
+    is Empty -> defaultValue
     is Loading -> defaultValue
     is Success -> value
     is Failure -> defaultValue
 }
 
 inline fun <T, R> UIState<T>.map(transform: (value: T) -> R): UIState<R> = when (this) {
+    is Empty -> Empty
     is Loading -> Loading
     is Success -> Success(transform(value))
     is Failure -> Failure(exception)
 }
 
 inline fun <R, T> UIState<T>.fold(
+    onEmpty: () -> R,
     onLoading: () -> R,
     onSuccess: (value: T) -> R,
     onFailure: (exception: Throwable) -> R,
 ): R = when (this) {
+    is Empty -> onEmpty()
     is Loading -> onLoading()
     is Success -> onSuccess(value)
     is Failure -> onFailure(exception)
@@ -62,3 +68,5 @@ inline fun <T> UIState<T>.onLoading(action: () -> Unit): UIState<T> {
     if (this is Loading) action()
     return this
 }
+
+fun <T> UIState<T>.asSuccess() = this as Success
