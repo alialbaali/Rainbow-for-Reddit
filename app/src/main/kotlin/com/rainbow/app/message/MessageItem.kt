949c6de385e4e76290bd851b3d4f8fd7c95b2b6d
Ali Albaali <ali@albaali.com>
2021-11-11 21:38:13 +0300

    Add MessageItem and MessagesScreen

--- app/src/main/kotlin/com/rainbow/app/message/MessageItem.kt
+++ app/src/main/kotlin/com/rainbow/app/message/MessageItem.kt
@@ -1,10 +1,37 @@
 package com.rainbow.app.message
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rainbow.app.components.UserName
+import com.rainbow.app.utils.ShapeModifier
+import com.rainbow.app.utils.defaultPadding
 import com.rainbow.domain.models.Message
 
 @Composable
-fun MessageItem(message: Message, modifier: Modifier = Modifier) {
-
+inline fun MessageItem(
+    message: Message,
+    crossinline onClick: (Message) -> Unit,
+    crossinline onUserNameClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier
+            .then(ShapeModifier)
+            .clickable { onClick(message) }
+            .defaultPadding()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        UserName(message.userName, onUserNameClick)
+        Text(message.subject, fontWeight = FontWeight.Medium, fontSize = 18.sp)
+        Text(message.body)
+    }
 }
--- app/src/main/kotlin/com/rainbow/app/message/MessagesScreen.kt
+++ app/src/main/kotlin/com/rainbow/app/message/MessagesScreen.kt
@@ -1,8 +1,67 @@
 package com.rainbow.app.message
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.rainbow.app.components.DefaultTabRow
+import com.rainbow.app.utils.OneTimeEffect
+import com.rainbow.app.utils.UIState
+import com.rainbow.app.utils.composed
+import com.rainbow.app.utils.toUIState
+import com.rainbow.data.Repos
+import com.rainbow.domain.models.Message
+
+enum class MessageTab {
+    Inbox, Unread, Sent, Messages, Mentions, PostMessages, CommentMessages;
+
+    companion object {
+        val Default = Inbox
+    }
+}
 
 @Composable
-fun MessagesScreen() {
+inline fun MessagesScreen(
+    crossinline onMessageClick: (Message) -> Unit,
+    crossinline onUserNameClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val scrollingState = rememberLazyListState()
+    var selectedTab by remember { mutableStateOf(MessageTab.Default) }
+    val state by produceState<UIState<List<Message>>>(UIState.Loading, selectedTab) {
+        value = when (selectedTab) {
+            MessageTab.Inbox -> Repos.Message.getInboxMessages()
+            MessageTab.Unread -> Repos.Message.getUnreadMessages()
+            MessageTab.Sent -> Repos.Message.getSentMessages()
+            MessageTab.Messages -> Repos.Message.getMessages()
+            MessageTab.Mentions -> Repos.Message.getMentions()
+            MessageTab.PostMessages -> Repos.Message.getPostMessages()
+            MessageTab.CommentMessages -> Repos.Message.getCommentMessages()
+        }.toUIState()
+    }
+    state.composed(modifier) { messages ->
+        OneTimeEffect(messages) {
+            messages.firstOrNull()?.let { onMessageClick(it) }
+        }
 
+        LazyColumn(modifier, scrollingState, verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            item {
+                DefaultTabRow(
+                    selectedTab = selectedTab,
+                    onTabClick = { selectedTab = it },
+                    Modifier.fillParentMaxWidth()
+                )
+            }
+            items(messages) { message ->
+                MessageItem(message, onMessageClick, onUserNameClick)
+            }
+        }
+
+        VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
+    }
 }
