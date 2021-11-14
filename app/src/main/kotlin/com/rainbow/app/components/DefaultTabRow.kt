b4659a563585a5841204074cbe146f12417bdd67
Ali Albaali <ali@albaali.com>
2021-11-14 04:56:03 +0300

    Add DefaultTabRow

---
+++ app/src/main/kotlin/com/rainbow/app/components/DefaultTabRow.kt
@@ -0,0 +1,36 @@
+package com.rainbow.app.components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rainbow.app.utils.ShapeModifier
+
+@Composable
+inline fun <reified T: Enum<T>> DefaultTabRow(
+    selectedTab: T,
+    crossinline onTabClick: (T) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val values = enumValues<T>()
+    ScrollableTabRow(
+        values.indexOf(selectedTab),
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = modifier
+            .then(ShapeModifier),
+        divider = {},
+        contentColor = MaterialTheme.colors.primary,
+    ) {
+        values.forEach { tab ->
+            Tab(
+                selected = tab == selectedTab,
+                onClick = { onTabClick(tab) },
+                text = {
+                    Text(tab.name)
+                }
+            )
+        }
+    }
+}
