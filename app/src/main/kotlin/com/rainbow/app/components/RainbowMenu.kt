e1660a1b5e7200e133d47455c3f0c15291a585fc
Ali Albaali <ali@albaali.com>
2021-11-14 04:56:15 +0300

    Add RainbowMenu

--- app/src/main/kotlin/com/rainbow/app/components/RainbowMenu.kt
+++ app/src/main/kotlin/com/rainbow/app/components/RainbowMenu.kt
@@ -1,15 +1,13 @@
 package com.rainbow.app.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.DpOffset
@@ -41,14 +39,8 @@ fun RainbowMenuItem(
     onclick: () -> Unit,
 ) {
     DropdownMenuItem(onclick, modifier, enabled) {
-        Row(
-            Modifier
-                .fillMaxSize(),
-            Arrangement.SpaceBetween,
-            Alignment.CenterVertically,
-        ) {
-            Icon(imageVector, contentDescription = text)
-            Text(text)
-        }
+        Icon(imageVector, contentDescription = text)
+        Spacer(Modifier.width(8.dp))
+        Text(text)
     }
 }
