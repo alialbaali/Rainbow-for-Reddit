774572dbc944b5921af9a3eed93f4eb8ea9f55b3
Ali Albaali <ali@albaali.com>
2021-11-14 04:56:56 +0300

    Add HeaderItem and CreationDate composables

---
+++ app/src/main/kotlin/com/rainbow/app/components/CreationDate.kt
@@ -0,0 +1,21 @@
+package com.rainbow.app.components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.rainbow.app.utils.displayTime
+import kotlinx.datetime.LocalDateTime
+
+@Composable
+fun CreationDate(date: LocalDateTime, modifier: Modifier = Modifier) {
+    Text(
+        date.displayTime,
+        modifier,
+        color = MaterialTheme.colors.onBackground.copy(0.5F),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+    )
+}
