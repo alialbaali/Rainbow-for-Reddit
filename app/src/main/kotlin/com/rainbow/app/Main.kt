c3617eb4ab10853c9b36b284441eccef4015b8cc
Ali Albaali <ali@albaali.com>
2021-11-15 07:04:13 +0300

    Refactor main function

--- app/src/main/kotlin/com/rainbow/app/Main.kt
+++ app/src/main/kotlin/com/rainbow/app/Main.kt
@@ -1,9 +1,6 @@
 package com.rainbow.app
 
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.application
 import com.arkivanov.decompose.extensions.compose.jetbrains.Children
 import com.arkivanov.decompose.router.push
@@ -15,52 +12,55 @@ import com.rainbow.app.utils.RainbowStrings
 import com.rainbow.data.Repos
 
 fun main() = application {
+    val isUserLoggedIn by Repos.User.isUserLoggedIn.collectAsState(false)
+    RainbowWindow(RainbowStrings.Rainbow) {
+        if (isUserLoggedIn)
+            ContentScreen()
+        else
+            LoginScreen()
+    }
+}
 
+@Composable
+private fun ContentScreen() {
     val router = rememberRouter(
         initialConfiguration = { Screen.SidebarItem.Home },
         configurationClass = Screen::class,
     )
-    val isUserLoggedIn by Repos.User.isUserLoggedIn.collectAsState(false)
     val forwardStack by remember { mutableStateOf<MutableList<Screen>>(mutableListOf()) }
-
-    RainbowWindow(RainbowStrings.Rainbow) {
-        if (isUserLoggedIn)
-            Children(router.state) { child ->
-                Rainbow(
-                    screen = child.configuration,
-                    backStack = router.state.value.backStack.map { it.configuration },
-                    onSidebarClick = {
-                        router.navigate { screens ->
-                            screens.filterNot { it is Screen.SidebarItem } + it
-                        }
-                    },
-                    onUserNameClick = { userName ->
-                        if ((child.configuration as? Screen.User)?.userName != userName) {
-                            router.push(Screen.User(userName))
-                            forwardStack.clear()
-                        }
-                    },
-                    onSubredditNameClick = { subredditName ->
-                        if ((child.configuration as? Screen.Subreddit)?.subredditName != subredditName) {
-                            router.push(Screen.Subreddit(subredditName))
-                            forwardStack.clear()
-                        }
-                    },
-                    onBackClick = {
-                        router.navigate {
-                            forwardStack += it.last()
-                            it.dropLast(1)
-                        }
-                    },
-                    onForwardClick = {
-                        router.push(forwardStack.last())
-                        forwardStack.removeLast()
-                    },
-                    isBackEnabled = router.state.value.backStack.isNotEmpty(),
-                    isForwardEnabled = forwardStack.isNotEmpty(),
-                )
-            }
-        else
-            LoginScreen()
+    Children(router.state) { child ->
+        Rainbow(
+            screen = child.configuration,
+            backStack = router.state.value.backStack.map { it.configuration },
+            onSidebarClick = {
+                router.navigate { screens ->
+                    screens.filterNot { it is Screen.SidebarItem } + it
+                }
+            },
+            onUserNameClick = { userName ->
+                if ((child.configuration as? Screen.User)?.userName != userName) {
+                    router.push(Screen.User(userName))
+                    forwardStack.clear()
+                }
+            },
+            onSubredditNameClick = { subredditName ->
+                if ((child.configuration as? Screen.Subreddit)?.subredditName != subredditName) {
+                    router.push(Screen.Subreddit(subredditName))
+                    forwardStack.clear()
+                }
+            },
+            onBackClick = {
+                router.navigate {
+                    forwardStack += it.last()
+                    it.dropLast(1)
+                }
+            },
+            onForwardClick = {
+                router.push(forwardStack.last())
+                forwardStack.removeLast()
+            },
+            isBackEnabled = router.state.value.backStack.isNotEmpty(),
+            isForwardEnabled = forwardStack.isNotEmpty(),
+        )
     }
 }
