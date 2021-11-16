1b06826498c58127bd77f9d56a272cfc905e809c
Ali Albaali <ali@albaali.com>
2021-11-16 04:26:21 +0300

    Add animations to vote actions

--- app/src/main/kotlin/com/rainbow/app/components/VoteActions.kt
+++ app/src/main/kotlin/com/rainbow/app/components/VoteActions.kt
@@ -1,17 +1,19 @@
 package com.rainbow.app.components
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.rainbow.app.utils.defaultShape
 import com.rainbow.domain.models.Vote
 
 @Composable
@@ -24,11 +26,36 @@ fun VoteActions(
     modifier: Modifier = Modifier,
 ) {
 
-    Row(
-        modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+    val borderColor by animateColorAsState(
+        when (vote) {
+            Vote.Up -> MaterialTheme.colors.primary
+            Vote.Down -> MaterialTheme.colors.secondary
+            Vote.None -> MaterialTheme.colors.onBackground.copy(0.1F)
+        }
+    )
 
+    val contentColor by animateColorAsState(
+        when (vote) {
+            Vote.Up -> MaterialTheme.colors.primary
+            Vote.Down -> MaterialTheme.colors.secondary
+            Vote.None -> MaterialTheme.colors.onBackground
+        }
+    )
+
+    val updatedVotesCount by animateIntAsState(
+        when (vote) {
+            Vote.Up -> votesCount.inc()
+            Vote.Down -> votesCount.dec()
+            Vote.None -> votesCount
+        }.toInt()
+    )
+
+    Row(
+        modifier
+            .defaultShape(borderColor = borderColor, shape = CircleShape),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         UpvoteButton(
             onClick = {
                 when (vote) {
@@ -36,32 +63,9 @@ fun VoteActions(
                     else -> onUpvote()
                 }
             },
-            tint = when (vote) {
-                Vote.Up -> MaterialTheme.colors.primary
-                else -> MaterialTheme.colors.onBackground
-            },
-            modifier = Modifier
-                .background(
-                    when (vote) {
-                        Vote.Up -> MaterialTheme.colors.primary.copy(0.1F)
-                        else -> MaterialTheme.colors.background
-                    },
-                    CircleShape,
-                )
+            tint = contentColor,
         )
-
-        Spacer(Modifier.width(8.dp))
-
-        val updatedVotesCount = when (vote) {
-            Vote.Up -> votesCount.inc()
-            Vote.Down -> votesCount.dec()
-            Vote.None -> votesCount
-        }
-
-        Text(updatedVotesCount.toString(), fontSize = 14.sp)
-
-        Spacer(Modifier.width(8.dp))
-
+        Text(updatedVotesCount.toString(), fontSize = 14.sp, color = contentColor)
         DownvoteButton(
             onClick = {
                 when (vote) {
@@ -69,18 +73,7 @@ fun VoteActions(
                     else -> onDownvote()
                 }
             },
-            tint = when (vote) {
-                Vote.Down -> MaterialTheme.colors.secondary
-                else -> MaterialTheme.colors.onBackground
-            },
-            modifier = Modifier
-                .background(
-                    when (vote) {
-                        Vote.Down -> MaterialTheme.colors.secondary.copy(0.1F)
-                        else -> MaterialTheme.colors.background
-                    },
-                    CircleShape,
-                )
+            tint = contentColor,
         )
     }
 }
