c12508a6f14e37fad6109e02dc57e50ba8ff73a7
Ali Albaali <ali@albaali.com>
2021-11-16 05:01:31 +0300

    Refactor award models and UI

--- app/src/main/kotlin/com/rainbow/app/award/Awards.kt
+++ app/src/main/kotlin/com/rainbow/app/award/Awards.kt
@@ -1,39 +1,51 @@
 package com.rainbow.app.award
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import com.rainbow.app.utils.RainbowStrings
 import com.rainbow.domain.models.Award
 
 @Composable
 fun Awards(awards: List<Award>) {
-    Row(
-        modifier = Modifier
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.primaryVariant.copy(0.05F))
-            .border(2.dp, MaterialTheme.colors.primaryVariant.copy(0.1F)),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        awards
-            .distinct()
-            .onEach { award ->
-                Award(
-                    award,
-                    modifier = Modifier
-                        .size(24.dp)
-                        .padding(2.dp),
-                )
-            }
-        Spacer(modifier = Modifier.width(4.dp))
-        Text(awards.count().toString())
+
+    val interactionSource = MutableInteractionSource()
+    val isHovered by interactionSource.collectIsHoveredAsState()
+    Column(Modifier.wrapContentSize()) {
+        Row(
+            modifier = Modifier
+                .hoverable(interactionSource),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            awards.distinct()
+                .onEach { award ->
+                    Award(
+                        award,
+                        modifier = Modifier.size(20.dp),
+                    )
+                }
+        }
+        DropdownMenu(isHovered, onDismissRequest = {}, focusable = false) {
+            awards.distinct()
+                .forEach { award ->
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Award(award, Modifier.size(24.dp))
+                        Text(awards.count().toString())
+                    }
+                }
+        }
     }
 }
--- remote/src/main/kotlin/com/rainbow/remote/dto/RemoteAward.kt
+++ remote/src/main/kotlin/com/rainbow/remote/dto/RemoteAward.kt
@@ -1,46 +1,69 @@
 package com.rainbow.remote.dto
 
+
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-
 @Serializable
 data class RemoteAward(
-    val id: String,
-
-    val name: String,
-
-    val description: String,
-
-    val count: Long,
-
-    @SerialName("coin_price")
-    val coinPrice: Long,
-
     @SerialName("award_sub_type")
-    val awardSubType: AwardSubType? = null,
-
+    val awardSubType: String? = null, // GLOBAL
+    @SerialName("award_type")
+    val awardType: String? = null, // global
+//    @SerialName("awardings_required_to_grant_benefits")
+//    val awardingsRequiredToGrantBenefits: Any? = null, // null
+    @SerialName("coin_price")
+    val coinPrice: Int? = null, // 100
+    @SerialName("coin_reward")
+    val coinReward: Int? = null, // 0
+    @SerialName("count")
+    val count: Int? = null, // 10
+    @SerialName("days_of_drip_extension")
+    val daysOfDripExtension: Int? = null, // 0
+    @SerialName("days_of_premium")
+    val daysOfPremium: Int? = null, // 0
+    @SerialName("description")
+    val description: String? = null, // Shows the Silver Award... and that's it.
+//    @SerialName("end_date")
+//    val endDate: Any? = null, // null
+    @SerialName("giver_coin_reward")
+    val giverCoinReward: Int? = null, // null
+    @SerialName("icon_format")
+    val iconFormat: String? = null, // null
+    @SerialName("icon_height")
+    val iconHeight: Int? = null, // 512
     @SerialName("icon_url")
-    val IconUrl: String,
-
+    val iconUrl: String? = null, // https://www.redditstatic.com/gold/awards/icon/silver_512.png
+    @SerialName("icon_width")
+    val iconWidth: Int? = null, // 512
+    @SerialName("id")
+    val id: String? = null, // gid_1
+    @SerialName("is_enabled")
+    val isEnabled: Boolean? = null, // true
+    @SerialName("is_new")
+    val isNew: Boolean? = null, // false
+    @SerialName("name")
+    val name: String? = null, // Silver
+    @SerialName("penny_donate")
+    val pennyDonate: Int? = null, // null
+    @SerialName("penny_price")
+    val pennyPrice: Int? = null, // null
+//    @SerialName("resized_icons")
+//    val resizedIcons: List<Any>? = null,
+//    @SerialName("resized_static_icons")
+//    val resizedStaticIcons: List<Any>? = null,
+//    @SerialName("start_date")
+//    val startDate: Any? = null, // null
+    @SerialName("static_icon_height")
+    val staticIconHeight: Int? = null, // 512
     @SerialName("static_icon_url")
-    val staticIconUrl: String,
-) {
-    @Serializable
-    enum class AwardSubType {
-        @SerialName("GLOBAL")
-        Global,
-
-        @SerialName("PREMIUM")
-        Premium,
-
-        @SerialName("APPRECIATION")
-        Appreciation,
-
-        @SerialName("GROUP")
-        Group,
-
-        @SerialName("COMMUNITY")
-        Community,
-    }
-}
+    val staticIconUrl: String? = null, // https://www.redditstatic.com/gold/awards/icon/silver_512.png
+    @SerialName("static_icon_width")
+    val staticIconWidth: Int? = null, // 512
+    @SerialName("subreddit_coin_reward")
+    val subredditCoinReward: Int? = null, // 0
+//    @SerialName("subreddit_id")
+//    val subredditId: Any? = null, // null
+//    @SerialName("tiers_by_required_awardings")
+//    val tiersByRequiredAwardings: Any? = null // null
+)
