5053e7c3f3c5146d3bfe841e6906f1da203589fb
Ali Albaali <ali@albaali.com>
2021-11-21 13:31:53 +0300

    Ignore content headers when parsing images

--- app/src/main/kotlin/com/rainbow/app/utils/JsoupUtils.kt
+++ app/src/main/kotlin/com/rainbow/app/utils/JsoupUtils.kt
@@ -3,6 +3,8 @@ package com.rainbow.app.utils
 import org.jsoup.Jsoup
 
 val String.imageUrl
-    get() = Jsoup.connect(this).get().select("img")
+    get() = Jsoup.connect(this)
+        .ignoreContentType(true)
+        .get().select("img")
         .firstOrNull()
         ?.absUrl("src")
