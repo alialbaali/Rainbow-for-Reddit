831c5fa73227c679b89b59b163284e1f9f15c4ea
Ali Albaali <ali@albaali.com>
2021-11-21 13:31:35 +0300

    Fix search

--- app/src/main/kotlin/com/rainbow/app/search/Search.kt
+++ app/src/main/kotlin/com/rainbow/app/search/Search.kt
@@ -1,24 +1,24 @@
 package com.rainbow.app.search
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
-import com.rainbow.app.utils.RainbowIcons
-import com.rainbow.app.utils.RainbowStrings
-import com.rainbow.app.utils.UIState
-import com.rainbow.app.utils.toUIState
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.dp
+import com.rainbow.app.components.RainbowProgressIndicator
+import com.rainbow.app.utils.*
 import com.rainbow.data.Repos
 import com.rainbow.domain.models.Subreddit
 import com.rainbow.domain.models.SubredditsSearchSorting
+import io.kamel.image.KamelImage
+import io.kamel.image.lazyPainterResource
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 
@@ -29,16 +29,16 @@ fun SearchTextField(
     onSubredditNameClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var isExpanded by remember { mutableStateOf(false) }
     var searchTerm by remember { mutableStateOf("") }
     val state by produceState<UIState<List<Subreddit>>>(UIState.Empty, searchTerm) {
         if (searchTerm.isNotBlank()) {
+            value = UIState.Loading
             Repos.Subreddit.searchSubreddit(searchTerm, SubredditsSearchSorting.Activity)
                 .map { it.toUIState() }
                 .collect { value = it }
-            isExpanded = true
         }
     }
+    var width by remember { mutableStateOf(0) }
 
     Column(modifier) {
         OutlinedTextField(
@@ -51,37 +51,54 @@ fun SearchTextField(
                 }
             },
             singleLine = true,
-            modifier = Modifier.onKeyEvent {
-                if (it.key == Key.Enter)
-                    searchTerm.takeIf { it.isNotBlank() }?.let(onSearchClick)
-                false
-            }
+            modifier = modifier
+                .onSizeChanged { width = it.width }
+                .onKeyEvent {
+                    if (it.key == Key.Enter)
+                        searchTerm.takeIf { it.isNotBlank() }?.let(onSearchClick)
+                    false
+                }
         )
-//        DropdownMenu(true, onDismissRequest = { isExpanded = false }) {
-//            when (state) {
-//                is UIState.Loading -> {
-//                    RainbowProgressIndicator()
-//                }
-//                is UIState.Success -> {
-//                    state.asSuccess().value.onEach {
-//                        DropdownMenuItem(
-//                            onClick = {
-//                                onSubredditNameClick(it.name)
+        DropdownMenu(
+            searchTerm.isNotBlank(),
+            onDismissRequest = { },
+            focusable = false,
+            Modifier.width(width.dp)
+        ) {
+            when (state) {
+                is UIState.Loading -> {
+                    RainbowProgressIndicator()
+                }
+                is UIState.Success -> {
+                    state.asSuccess().value
+                        .takeIf { it.isNotEmpty() }
+                        ?.take(10)
+                        ?.onEach {
+                            DropdownMenuItem(
+                                onClick = {
+                                    onSubredditNameClick(it.name)
 //                                isExpanded = false
-//                            }
-//                        ) {
-//                            Text(it.name)
-//                        }
-//                    }
-//                }
-//                is UIState.Failure -> {
-//                    Text("Failed loading subreddits.")
-//                }
-//                UIState.Empty -> {
-//                    Text("Loading")
-//                }
-//            }
-//        }
+                                }
+                            ) {
+                                val painterResource = lazyPainterResource(it.imageUrl ?: it.bannerImageUrl.toString())
+                                KamelImage(
+                                    painterResource,
+                                    it.name,
+                                    Modifier.size(24.dp).clip(MaterialTheme.shapes.small)
+                                )
+                                Spacer(Modifier.width(16.dp))
+                                Text(it.name)
+                            }
+                        } ?: Text("No matching subreddits.", Modifier.padding(16.dp))
+                }
+                is UIState.Failure -> {
+                    Text("Failed loading subreddits.")
+                }
+                UIState.Empty -> {
+                    Text("Loading")
+                }
+            }
+        }
     }
 
 }
