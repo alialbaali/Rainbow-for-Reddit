5384acf0c4fd4f99d9ce546736d33eec52738748
Ali Albaali <ali@albaali.com>
2021-12-10 21:49:43 +0300

    Extract LazyGrid implementation

--- app/src/main/kotlin/com/rainbow/app/subreddit/Subreddits.kt
+++ app/src/main/kotlin/com/rainbow/app/subreddit/Subreddits.kt
@@ -1,17 +1,12 @@
 package com.rainbow.app.subreddit
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.dp
+import com.rainbow.app.components.LazyGrid
 import com.rainbow.app.utils.RainbowStrings
 import com.rainbow.app.utils.defaultPadding
 import com.rainbow.domain.models.Subreddit
@@ -26,10 +21,8 @@ fun Subreddits(
     onClick: (Subreddit) -> Unit,
     onLoadMore: (Subreddit) -> Unit,
     onShowSnackbar: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    var cellCount by remember { mutableStateOf(3) }
-    val scrollingState = rememberLazyListState()
     var searchTerm by remember { mutableStateOf("") }
     val subreddits = subreddits.let {
         searchTerm.filter { !it.isWhitespace() }
@@ -41,21 +34,8 @@ fun Subreddits(
             } ?: subreddits
     }
 
-    LazyColumn(
-        modifier
-            .padding(horizontal = 16.dp)
-            .onSizeChanged { size ->
-                cellCount = when {
-                    size.width < 1000 -> 2
-                    size.width in 1000 until 1500 -> 3
-                    else -> 4
-                }
-            },
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        state = scrollingState,
-    ) {
-        val rows = if (subreddits.isEmpty()) 0 else 1 + (subreddits.size - 1) / cellCount
-        item {
+    LazyGrid(
+        item = {
             OutlinedTextField(
                 value = searchTerm,
                 onValueChange = { searchTerm = it },
@@ -63,30 +43,12 @@ fun Subreddits(
                 placeholder = { Text(RainbowStrings.FilterSubreddits) },
             )
         }
-        items(rows) { rowIndex ->
-            Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                for (columnIndex in 0 until cellCount) {
-                    val itemIndex = rowIndex * cellCount + columnIndex
-                    if (itemIndex < subreddits.size) {
-                        Box(
-                            modifier = Modifier.weight(1f, fill = true),
-                            propagateMinConstraints = true
-                        ) {
-                            when (subredditType) {
-                                SubredditType.Default -> SubredditItem(subreddits[itemIndex], onClick, onShowSnackbar)
-                                SubredditType.Search -> SearchSubredditItem(
-                                    subreddits[itemIndex],
-                                    onClick,
-                                    onShowSnackbar
-                                )
-                            }
-                        }
-                    } else {
-                        Spacer(Modifier.weight(1f, fill = true))
-                    }
-                }
+    ) {
+        items(subreddits) { subreddit ->
+            when (subredditType) {
+                SubredditType.Default -> SubredditItem(subreddit, onClick, onShowSnackbar)
+                SubredditType.Search -> SearchSubredditItem(subreddit, onClick, onShowSnackbar)
             }
         }
     }
-    VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
 }
--- app/src/main/kotlin/com/rainbow/app/user/Users.kt
+++ app/src/main/kotlin/com/rainbow/app/user/Users.kt
@@ -1,20 +1,10 @@
 package com.rainbow.app.user
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.dp
-import com.rainbow.app.subreddit.SubredditType
-import com.rainbow.app.utils.RainbowStrings
-import com.rainbow.app.utils.defaultPadding
+import com.rainbow.app.components.LazyGrid
 import com.rainbow.domain.models.User
 
 enum class UserType { Default, Search }
@@ -29,65 +19,12 @@ fun Users(
     onShowSnackbar: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var cellCount by remember { mutableStateOf(3) }
-    val scrollingState = rememberLazyListState()
-    var searchTerm by remember { mutableStateOf("") }
-    val subreddits = users.let {
-        searchTerm.filter { !it.isWhitespace() }
-            .takeIf { it.isNotBlank() }
-            ?.let { searchTerm ->
-                users.filter {
-                    it.name.contains(searchTerm, ignoreCase = true) || it.description.orEmpty().contains(searchTerm)
-                }
-            } ?: users
-    }
-
-    LazyColumn(
-        modifier
-            .padding(horizontal = 16.dp)
-            .onSizeChanged { size ->
-                cellCount = when {
-                    size.width < 1000 -> 2
-                    size.width in 1000 until 1500 -> 3
-                    else -> 4
-                }
-            },
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        state = scrollingState,
-    ) {
-        val rows = if (subreddits.isEmpty()) 0 else 1 + (subreddits.size - 1) / cellCount
-        item {
-            OutlinedTextField(
-                value = searchTerm,
-                onValueChange = { searchTerm = it },
-                modifier = Modifier.defaultPadding(),
-                placeholder = { Text(RainbowStrings.FilterSubreddits) },
-            )
-        }
-        items(rows) { rowIndex ->
-            Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                for (columnIndex in 0 until cellCount) {
-                    val itemIndex = rowIndex * cellCount + columnIndex
-                    if (itemIndex < subreddits.size) {
-                        Box(
-                            modifier = Modifier.weight(1f, fill = true),
-                            propagateMinConstraints = true
-                        ) {
-                            when (userType) {
-                                UserType.Default -> UserItem(subreddits[itemIndex], onClick, onShowSnackbar)
-                                UserType.Search -> UserItem(
-                                    subreddits[itemIndex],
-                                    onClick,
-                                    onShowSnackbar
-                                )
-                            }
-                        }
-                    } else {
-                        Spacer(Modifier.weight(1f, fill = true))
-                    }
-                }
+    LazyGrid {
+        items(users) { user ->
+            when (userType) {
+                UserType.Default -> UserItem(user, onClick, onShowSnackbar)
+                UserType.Search -> UserItem(user, onClick, onShowSnackbar)
             }
         }
     }
-    VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
 }
