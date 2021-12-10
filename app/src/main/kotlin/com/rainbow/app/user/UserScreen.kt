24655e576159f0c8130a1c58df5adbbd60084e1e
Ali Albaali <ali@albaali.com>
2021-12-10 16:47:39 +0300

    Refactor UserScreen to use UserScreenModel

--- app/src/main/kotlin/com/rainbow/app/user/UserScreen.kt
+++ app/src/main/kotlin/com/rainbow/app/user/UserScreen.kt
@@ -1,74 +1,90 @@
 package com.rainbow.app.user
 
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.unit.dp
-import com.rainbow.app.comment.comments
+import com.rainbow.app.comment.userComments
 import com.rainbow.app.components.DefaultTabRow
-import com.rainbow.app.post.PostModel
+import com.rainbow.app.components.RainbowLazyColumn
+import com.rainbow.app.item.items
+import com.rainbow.app.model.ListModel
 import com.rainbow.app.post.posts
 import com.rainbow.app.profile.Header
-import com.rainbow.app.utils.UIState
 import com.rainbow.app.utils.composed
 import com.rainbow.domain.models.Comment
-import com.rainbow.domain.models.PostLayout
-import com.rainbow.domain.models.PostSorting
+import com.rainbow.domain.models.Post
 
-private enum class UserTab {
-    Overview, Submitted, Comments,
+enum class UserTab {
+    Overview, Submitted, Comments;
+
+    companion object {
+        val Default = Overview
+    }
 }
 
 @Composable
 fun UserScreen(
     userName: String,
+    onPostUpdate: (Post) -> Unit,
+    onPostClick: (Post) -> Unit,
+    onCommentClick: (Comment) -> Unit,
     focusRequester: FocusRequester,
     onUserNameClick: (String) -> Unit,
     onSubredditNameClick: (String) -> Unit,
     onShowSnackbar: (String) -> Unit,
-    setPostModel: (PostModel<PostSorting>) -> Unit,
+    setPostModel: (ListModel<*>) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var selectedTab by remember { mutableStateOf(UserTab.Submitted) }
-    val scrollingState = rememberLazyListState()
-    val model = remember { UserModel.getOrCreateInstance(userName) }
-    setPostModel(model.postModel as PostModel<PostSorting>)
-    val postLayout by model.postModel.postLayout.collectAsState(PostLayout.Card)
+    val model = remember(userName) { UserScreenModel.getOrCreateInstance(userName) }
+    setPostModel(model.itemListModel)
     val userState by model.user.collectAsState()
-    val postsState by model.postModel.posts.collectAsState()
-    val commentsState by produceState<UIState<List<Comment>>>(UIState.Loading) {
-//            Repos.Comment.getUserComments(user.id)
-    }
+    val selectedTab by model.selectedTab.collectAsState()
+    val postLayout by model.itemListModel.postLayout.collectAsState()
+    val itemsState by model.itemListModel.items.collectAsState()
+    val commentState by model.commentListModel.items.collectAsState()
+    val postsState by model.postListModel.items.collectAsState()
     userState.composed(onShowSnackbar) { user ->
-        LazyColumn(modifier, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        RainbowLazyColumn(modifier) {
             item { Header(user) }
             item {
                 DefaultTabRow(
                     selectedTab = selectedTab,
-                    onTabClick = { selectedTab = it }
+                    onTabClick = { model.selectTab(it) }
                 )
             }
             when (selectedTab) {
-                UserTab.Overview -> {
-                }
+                UserTab.Overview -> items(
+                    itemsState,
+                    postLayout,
+                    focusRequester,
+                    onUserNameClick,
+                    onSubredditNameClick,
+                    onPostClick,
+                    onCommentClick,
+                    onPostUpdate,
+                    onShowSnackbar,
+                )
                 UserTab.Submitted -> posts(
                     postsState,
-                    model.postModel,
+                    onPostUpdate,
                     postLayout,
                     focusRequester,
                     onUserNameClick,
                     onSubredditNameClick,
                     onShowSnackbar,
-                    onLoadMore = { model.postModel.selectPost(it.id) }
+                    {},
+                    onPostClick
                 )
-                UserTab.Comments -> comments(commentsState, onUserNameClick, onSubredditNameClick)
+                UserTab.Comments -> userComments(
+                    commentState,
+                    onUserNameClick,
+                    onSubredditNameClick,
+                    onCommentClick
+                )
             }
         }
-        VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
     }
 }
--- app/src/main/kotlin/com/rainbow/app/user/UserScreenModel.kt
+++ app/src/main/kotlin/com/rainbow/app/user/UserScreenModel.kt
@@ -1,7 +1,9 @@
 package com.rainbow.app.user
 
-import com.rainbow.app.post.PostModel
-import com.rainbow.app.utils.Model
+import com.rainbow.app.comment.CommentListModel
+import com.rainbow.app.item.ItemListModel
+import com.rainbow.app.model.Model
+import com.rainbow.app.post.PostListModel
 import com.rainbow.app.utils.UIState
 import com.rainbow.app.utils.toUIState
 import com.rainbow.data.Repos
@@ -11,17 +13,26 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-private val userModels = mutableSetOf<UserModel>()
+private val userScreenModels = mutableSetOf<UserScreenModel>()
 
-class UserModel private constructor(private val userName: String) : Model() {
+class UserScreenModel private constructor(private val userName: String) : Model() {
 
     private val mutableSelectedTab = MutableStateFlow(UserTab.Default)
+    val selectedTab get() = mutableSelectedTab.asStateFlow()
 
     private val mutableUser = MutableStateFlow<UIState<User>>(UIState.Loading)
     val user get() = mutableUser.asStateFlow()
 
-    val postModel = PostModel(UserPostSorting.Top) { postSorting, timeSorting, lastPostId ->
+    val itemListModel = ItemListModel(UserPostSorting.Default) { postSorting, timeSorting, lastPostId ->
+        Repos.Item.getUserOverviewItems(userName, postSorting, timeSorting, lastPostId)
+    }
+
+    val postListModel = PostListModel(UserPostSorting.Default) { postSorting, timeSorting, lastPostId ->
         Repos.Post.getUserSubmittedPosts(userName, postSorting, timeSorting, lastPostId)
+    }
+
+    val commentListModel = CommentListModel(UserPostSorting.Default) { postSorting, timeSorting, lastPostId ->
+        Repos.Comment.getUserComments(userName)
     }
 
     init {
@@ -29,14 +40,23 @@ class UserModel private constructor(private val userName: String) : Model() {
     }
 
     companion object {
-        fun getOrCreateInstance(userName: String): UserModel {
-            return userModels.find { it.userName == userName } ?: UserModel(userName).also { userModels += it }
+        fun getOrCreateInstance(userName: String): UserScreenModel {
+            return userScreenModels.find { it.userName == userName }
+                ?: UserScreenModel(userName).also { userScreenModels += it }
         }
     }
 
     fun loadUser() = scope.launch {
         mutableUser.value = Repos.User.getUser(userName)
-            .onSuccess { postModel.loadPosts() }
+            .onSuccess {
+                itemListModel.loadItems()
+                postListModel.loadItems()
+                commentListModel.loadItems()
+            }
             .toUIState()
+    }
+
+    fun selectTab(tab: UserTab) {
+        mutableSelectedTab.value = tab
     }
 }
