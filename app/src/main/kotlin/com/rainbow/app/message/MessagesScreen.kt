b7b55c9179250277c5f2d5ccb02bbd33c95c252e
Ali Albaali <ali@albaali.com>
2021-12-18 08:10:55 +0300

    Add MessagesScreenModel and Refactor MessagesScreen

--- app/src/main/kotlin/com/rainbow/app/message/MessagesScreen.kt
+++ app/src/main/kotlin/com/rainbow/app/message/MessagesScreen.kt
@@ -1,20 +1,10 @@
 package com.rainbow.app.message
 
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.rainbow.app.components.DefaultTabRow
-import com.rainbow.app.utils.OneTimeEffect
-import com.rainbow.app.utils.UIState
-import com.rainbow.app.utils.composed
-import com.rainbow.app.utils.toUIState
-import com.rainbow.data.Repos
+import com.rainbow.app.components.RainbowLazyColumn
+import com.rainbow.app.model.ListModel
 import com.rainbow.domain.models.Message
 
 enum class MessageTab {
@@ -29,40 +19,55 @@ enum class MessageTab {
 inline fun MessagesScreen(
     crossinline onMessageClick: (Message) -> Unit,
     crossinline onUserNameClick: (String) -> Unit,
-    onShowSnackbar: (String) -> Unit,
-    modifier: Modifier = Modifier
+    noinline onShowSnackbar: (String) -> Unit,
+    crossinline setListModel: (ListModel<*>) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    val scrollingState = rememberLazyListState()
-    var selectedTab by remember { mutableStateOf(MessageTab.Default) }
-    val state by produceState<UIState<List<Message>>>(UIState.Loading, selectedTab) {
-        value = when (selectedTab) {
-            MessageTab.Inbox -> Repos.Message.getInboxMessages()
-            MessageTab.Unread -> Repos.Message.getUnreadMessages()
-            MessageTab.Sent -> Repos.Message.getSentMessages()
-            MessageTab.Messages -> Repos.Message.getMessages()
-            MessageTab.Mentions -> Repos.Message.getMentions()
-            MessageTab.PostMessages -> Repos.Message.getPostMessages()
-            MessageTab.CommentMessages -> Repos.Message.getCommentMessages()
-        }.toUIState()
-    }
-    state.composed(onShowSnackbar, modifier) { messages ->
-        OneTimeEffect(messages) {
-            messages.firstOrNull()?.let { onMessageClick(it) }
+    val selectedTab by MessagesScreenModel.selectedTab.collectAsState()
+    val inboxMessages by MessagesScreenModel.inboxMessages.items.collectAsState()
+    val unreadMessages by MessagesScreenModel.unreadMessages.items.collectAsState()
+    val sentMessages by MessagesScreenModel.sentMessages.items.collectAsState()
+    val messages by MessagesScreenModel.messages.items.collectAsState()
+    val mentions by MessagesScreenModel.mentions.items.collectAsState()
+    val postMessages by MessagesScreenModel.postMessages.items.collectAsState()
+    val commentMessages by MessagesScreenModel.commentMessages.items.collectAsState()
+    RainbowLazyColumn(modifier) {
+        item {
+            DefaultTabRow(
+                selectedTab = selectedTab,
+                onTabClick = { MessagesScreenModel.selectTab(it) },
+                Modifier.fillParentMaxWidth()
+            )
         }
-
-        LazyColumn(modifier, scrollingState, verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            item {
-                DefaultTabRow(
-                    selectedTab = selectedTab,
-                    onTabClick = { selectedTab = it },
-                    Modifier.fillParentMaxWidth()
-                )
+        when (selectedTab) {
+            MessageTab.Inbox -> {
+                setListModel(MessagesScreenModel.inboxMessages)
+                messages(inboxMessages, onMessageClick, onUserNameClick)
             }
-            items(messages) { message ->
-                MessageItem(message, onMessageClick, onUserNameClick)
+            MessageTab.Unread -> {
+                setListModel(MessagesScreenModel.unreadMessages)
+                messages(unreadMessages, onMessageClick, onUserNameClick)
+            }
+            MessageTab.Sent -> {
+                setListModel(MessagesScreenModel.sentMessages)
+                messages(sentMessages, onMessageClick, onUserNameClick)
+            }
+            MessageTab.Messages -> {
+                setListModel(MessagesScreenModel.messages)
+                messages(messages, onMessageClick, onUserNameClick)
+            }
+            MessageTab.Mentions -> {
+                setListModel(MessagesScreenModel.mentions)
+                messages(mentions, onMessageClick, onUserNameClick)
+            }
+            MessageTab.PostMessages -> {
+                setListModel(MessagesScreenModel.postMessages)
+                messages(postMessages, onMessageClick, onUserNameClick)
+            }
+            MessageTab.CommentMessages -> {
+                setListModel(MessagesScreenModel.commentMessages)
+                messages(commentMessages, onMessageClick, onUserNameClick)
             }
         }
-
-        VerticalScrollbar(rememberScrollbarAdapter(scrollingState))
     }
 }
