c6bfad6fc6f63c7a8eda35fe4365fb2defd162f9
Ali Albaali <ali@albaali.com>
2021-12-18 09:44:29 +0300

    Rename ImageBox to TextBox

--- app/src/main/kotlin/com/rainbow/app/components/TextBox.kt
+++ app/src/main/kotlin/com/rainbow/app/components/TextBox.kt
@@ -1,24 +1,20 @@
 package com.rainbow.app.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 
 @Composable
-fun TextImage(text: String, fontSize: TextUnit, modifier: Modifier = Modifier) {
+fun TextBox(text: String, fontSize: TextUnit, modifier: Modifier = Modifier) {
     Box(modifier, Alignment.Center) {
         Text(
             text.first().toString().uppercase(),
             color = MaterialTheme.colors.onSecondary,
             fontSize = fontSize,
-            modifier = Modifier.fillMaxSize().border(1.dp, MaterialTheme.colors.onBackground).align(Alignment.Center)
         )
     }
 }
