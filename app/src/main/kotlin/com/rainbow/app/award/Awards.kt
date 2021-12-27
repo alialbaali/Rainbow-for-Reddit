5552e803abb115a44ad3be3df58cb6e1f4a5d1cc
Ali Albaali <ali@albaali.com>
2021-12-27 12:10:17 +0300

    Use defaultBackgroundShape in Awards

--- app/src/main/kotlin/com/rainbow/app/award/Awards.kt
+++ app/src/main/kotlin/com/rainbow/app/award/Awards.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.rainbow.app.utils.defaultBackgroundShape
 import com.rainbow.domain.models.Award
 
 @Composable
@@ -19,7 +21,7 @@ fun Awards(awards: List<Award>, modifier: Modifier = Modifier) {
     val isHovered by interactionSource.collectIsHoveredAsState()
     var isDialogVisible by remember { mutableStateOf(false) }
     var selectedAward by remember { mutableStateOf(awards.firstOrNull()) }
-    Column(modifier) {
+    Column(modifier.defaultBackgroundShape(shape = CircleShape).padding(8.dp)) {
         Row(
             modifier = Modifier
                 .hoverable(interactionSource),
