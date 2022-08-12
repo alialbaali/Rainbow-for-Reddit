6754803370b4278f8e96f2652a0435fdc160e8cc
Ali Albaali <ali@albaali.com>
2022-08-12 21:53:46 +0300

    Use Nunito fonts

--- desktop/src/main/kotlin/com/rainbow/desktop/ui/Typography.kt
+++ desktop/src/main/kotlin/com/rainbow/desktop/ui/Typography.kt
@@ -1,24 +1,43 @@
 package com.rainbow.desktop.ui
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 
-val rubikFonts = listOf(
-    Font("Rubik/static/Rubik-Regular.ttf", FontWeight.Normal, FontStyle.Normal),
-    Font("Rubik/static/Rubik-SemiBold.ttf", FontWeight.SemiBold, FontStyle.Normal),
-    Font("Rubik/static/Rubik-SemiBoldItalic.ttf", FontWeight.SemiBold, FontStyle.Italic),
-    Font("Rubik/static/Rubik-ExtraBold.ttf", FontWeight.ExtraBold, FontStyle.Normal),
-    Font("Rubik/static/Rubik-ExtraBoldItalic.ttf", FontWeight.ExtraBold, FontStyle.Italic),
-    Font("Rubik/static/Rubik-Black.ttf", FontWeight.Black, FontStyle.Normal),
-    Font("Rubik/static/Rubik-BlackItalic.ttf", FontWeight.Black, FontStyle.Italic),
-    Font("Rubik/static/Rubik-Light.ttf", FontWeight.Light, FontStyle.Normal),
-    Font("Rubik/static/Rubik-LightItalic.ttf", FontWeight.Light, FontStyle.Italic),
-    Font("Rubik/static/Rubik-Medium.ttf", FontWeight.Medium, FontStyle.Normal),
-    Font("Rubik/static/Rubik-MediumItalic.ttf", FontWeight.Medium, FontStyle.Italic),
-    Font("Rubik/static/Rubik-Bold.ttf", FontWeight.Bold, FontStyle.Normal),
-    Font("Rubik/static/Rubik-BoldItalic.ttf", FontWeight.Bold, FontStyle.Italic),
+val nunitoFonts = listOf(
+    Font("font/Nunito-Regular.ttf", FontWeight.Normal, FontStyle.Normal),
+    Font("font/Nunito-SemiBold.ttf", FontWeight.SemiBold, FontStyle.Normal),
+    Font("font/Nunito-SemiBoldItalic.ttf", FontWeight.SemiBold, FontStyle.Italic),
+    Font("font/Nunito-ExtraBold.ttf", FontWeight.ExtraBold, FontStyle.Normal),
+    Font("font/Nunito-ExtraBoldItalic.ttf", FontWeight.ExtraBold, FontStyle.Italic),
+    Font("font/Nunito-Black.ttf", FontWeight.Black, FontStyle.Normal),
+    Font("font/Nunito-BlackItalic.ttf", FontWeight.Black, FontStyle.Italic),
+    Font("font/Nunito-Light.ttf", FontWeight.Light, FontStyle.Normal),
+    Font("font/Nunito-LightItalic.ttf", FontWeight.Light, FontStyle.Italic),
+    Font("font/Nunito-Medium.ttf", FontWeight.Medium, FontStyle.Normal),
+    Font("font/Nunito-MediumItalic.ttf", FontWeight.Medium, FontStyle.Italic),
+    Font("font/Nunito-Bold.ttf", FontWeight.Bold, FontStyle.Normal),
+    Font("font/Nunito-BoldItalic.ttf", FontWeight.Bold, FontStyle.Italic),
 )
 
-val typography: Typography = Typography()
+private val defaultTypography = Typography()
+
+val typography: Typography = Typography(
+    defaultTypography.displayLarge.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.displayMedium.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.displaySmall.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.headlineLarge.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.headlineMedium.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.headlineSmall.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.titleLarge.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.titleMedium.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.titleSmall.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.bodyLarge.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.bodyMedium.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.bodySmall.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.labelLarge.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.labelMedium.copy(fontFamily = FontFamily(nunitoFonts)),
+    defaultTypography.labelSmall.copy(fontFamily = FontFamily(nunitoFonts)),
+)
