93bff9ac04538636ed058cd293d9b7640f95c3b2
Ali Albaali <ali@albaali.com>
2022-09-28 02:54:09 +0300

    Fix getting comment side color by index

--- desktop/src/main/kotlin/com/rainbow/desktop/components/BoxLine.kt
+++ desktop/src/main/kotlin/com/rainbow/desktop/components/BoxLine.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.rainbow.desktop.ui.RainbowColors
@@ -15,7 +16,13 @@ fun BoxLine(
     index: Int,
     modifier: Modifier = Modifier,
 ) {
-    val color = RainbowColors.getOrNull(index) ?: RainbowColors[0]
+    val color = remember(index) {
+        if (index < RainbowColors.size) {
+            RainbowColors[index]
+        } else {
+            RainbowColors[index - RainbowColors.size]
+        }
+    }
     Box(
         modifier
             .background(color)
