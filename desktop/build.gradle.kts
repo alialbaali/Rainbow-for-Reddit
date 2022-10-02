1c7fdc3547b7f6129e7fb241fa1548661afbc248
Ali Albaali <ali@albaali.com>
2022-10-02 14:12:38 +0300

    Update Compose to 1.2.0-beta2

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose") version "1.2.0-beta01"
+    id("org.jetbrains.compose") version "1.2.0-beta02"
 }
 
 repositories {
